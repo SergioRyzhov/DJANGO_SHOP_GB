2f7370216f3765a07ea0b32a7611bd5b895c2754
Sergei Ryzhov <ryzhovs@tut.by>
2021-10-20 15:43:47 +0300

    Added basket model & add items process

--- basket/views.py
+++ basket/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponseRedirect
+from products.models import Product
+from basket.models import Basket
 
 # Create your views here.
+
+def basket_add(request, product_id):
+    product = Product.objects.get(id=product_id)
+    baskets = Basket.objects.filter(user=request.user, product=product)
+
+    if not baskets.exists():
+        Basket.objects.create(user=request.user, product=product)
+        return HttpResponseRedirect(request.META['HTTP_REFERER'])
+    else:
+        basket = baskets.first()
+        basket.quantity += 1
+        basket.save()
+        return HttpResponseRedirect(request.META['HTTP_REFERER'])
