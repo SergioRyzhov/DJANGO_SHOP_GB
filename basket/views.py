cf106723aa31406db6923e8970cea51b1b41cf5f
Sergei Ryzhov <ryzhovs@tut.by>
2021-10-20 17:22:03 +0300

    Added items quantity limit

--- basket/views.py
+++ basket/views.py
@@ -1,6 +1,8 @@
+from django.contrib import messages
 from django.shortcuts import HttpResponseRedirect
 from products.models import Product
 from basket.models import Basket
+from django.urls import reverse
 
 # Create your views here.
 
@@ -9,13 +11,17 @@ def basket_add(request, product_id):
     baskets = Basket.objects.filter(user=request.user, product=product)
 
     if not baskets.exists():
-        Basket.objects.create(user=request.user, product=product)
+        Basket.objects.create(user=request.user, product=product, quantity=1)
         return HttpResponseRedirect(request.META['HTTP_REFERER'])
     else:
         basket = baskets.first()
-        basket.quantity += 1
-        basket.save()
-        return HttpResponseRedirect(request.META['HTTP_REFERER'])
+        if basket.quantity <= product.quantity:
+            basket.quantity += 1
+            basket.save()
+            return HttpResponseRedirect(request.META['HTTP_REFERER'])
+        else:
+            messages.success(request, 'Количество заказанного товара превышает количество на складе')
+            return HttpResponseRedirect(reverse('users:profile'))
 
 
 def basket_remove(request, id):
