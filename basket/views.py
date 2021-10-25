4bee2e64926a682c7f33210c9a5c64d3491fe0b3
Sergei Ryzhov <ryzhovs@tut.by>
2021-10-25 10:21:26 +0300

    Add basket edit.
    Edit method change 6 files: urls.py, views.py, basket.html, base.html, profile.css, basket.js
    
    Basket.js contains ajax request to change item quantity and price

--- basket/views.py
+++ basket/views.py
@@ -4,6 +4,8 @@ from products.models import Product
 from basket.models import Basket
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from django.template.loader import render_to_string
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -31,3 +33,17 @@ def basket_remove(request, id):
     basket = Basket.objects.get(id=id)
     basket.delete()
     return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+@login_required
+def basket_edit(request, id, quantity):
+    if request.is_ajax():
+        basket = Basket.objects.get(id=id)
+        if quantity > 0:
+            basket.quantity = quantity
+            basket.save()
+        else:
+            basket.delete()
+    baskets = Basket.objects.filter(user=request.user)
+    context = {'baskets': baskets}
+    result = render_to_string('basket/basket.html', context)
+    return JsonResponse({'result': result})
